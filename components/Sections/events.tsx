6f409ab704d027f9562cc7003b7017bd8a158a28
Arun Thacharuthodi <67404902+arunthacharuthodi@users.noreply.github.com>
2022-11-02 18:39:02 +0530

    Update events.tsx

--- components/Sections/events.tsx
+++ components/Sections/events.tsx
@@ -1,158 +1,3 @@
-// import Icon from '@hackclub/icons'
-// import clsx from 'clsx'
-// import Heading from 'components/Heading'
-// import Image from 'next/image'
-// import arun from 'photos/team/arunthacharuthodi.jpg'
-// import Claire from 'photos/team/claire.png'
-// import Kunal from 'photos/team/kunal.jpeg'
-// import Theo from 'photos/team/theo.png'
-// import Sarthak from 'photos/team/sarthak.png'
-// import EventCard from 'components/event_card'
-// import Button from 'components/Button'
-// import Link from 'next/link'
-
-// const alumni = [
-//   'Jolene Pern',
-//   'Vicki Yang',
-//   'Jonathan Yin',
-//   'Kunal Sharda',
-//   'Matt Tengtrakool',
-//   'Michelle Yu',
-//   'Jocelyn Pern',
-//   'Justin Yu',
-//   'Max Krieger',
-//   'Edward Song',
-//   'Nimish Garg',
-//   'Jennifer Kim',
-//   'Katherine Huang',
-//   'Emily Tan',
-//   'Walter Shen',
-//   'Zhi Wei Gan',
-//   'Rebekah Agwunobi',
-//   'Aadhya Puttur',
-//   'Nina Zhang',
-// ]
-
-// const Event = ({ EventName, is_Active, eventType, desc, cost , is_Team , regLink }: any) => {
-//   if(is_Active){
-//     return (
-//         // <div className="flex items-center gap-3">
-//         //   <Image
-//         //     src={image}
-//         //     alt={name}
-//         //     width={120}
-//         //     height={120}
-//         //     className="rounded-md"
-//         //     objectFit="cover"
-//         //     objectPosition={'center'}
-//         //   />
-//         //   <div className="flex flex-col">
-//         //     <a href={link} target={'_blank'} rel="noreferrer">
-//         //       <h4
-//         //         className={clsx(
-//         //           'font-bold text-lg border-b border-transparent border-dashed',
-//         //           link && 'hover:border-primary hover:text-primary'
-//         //         )}
-//         //       >
-//         //         {name}
-//         //         {/* <span className="text-gray-500 font-light">({pronouns})</span> */}
-//         //       </h4>
-//         //     </a>
-//         //     <p className="text-gray-600">
-//         //       {role} &middot; {pronouns}
-//         //     </p>
-//         //     {/* <a><Icon glyph='web' /></a> */}
-//         //   </div>
-//         // </div>
-//         <div className=" " >
-
-// <div className="max-w-md bg-bg-card rounded-lg  shadow-md dark:bg-gray-800 dark:border-gray-700">
-    
-//       <img src="./img/posters/batteryws.png" alt="image"  />
-//     <div className="p-5">
-      
-//             <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-500 text-center dark:text-white max-w-full" >{EventName}</h5>
-      
-//         <p className="mb-3 font-ubuntu font-bold text-gray-700 dark:text-gray-400 ">{regLink}</p>
-//         <div className="cursor-not-allowed">
-//         <Link href={regLink}>
-//         <a >
-//         <Button  className="cursor-not-allowed hover:bg-zinc-500"  >
-//           Register
-//         </Button>
-//         </a>
-
-//         </Link>
-//         </div>
-//     </div>
-// </div>
-//     </div>
-//       )
-    
-//   }else{
-//       return(
-//         <EventCard/>
-//       );
-//   }
-// }
-
-// const Grid: React.FC = ({ children }) => (
-//   <div className="grid gap-5 grid-cols-1 md:grid-cols-3 grid-flow-row">
-//     {children}
-//   </div>
-// )
-
-// const EventSection: React.FC = () => {
-//   return (
-//     <>
-//       <Grid>
-
-//       <Event
-//       EventName="nothing"
-//       is_Active={false}
-
-//       />
-
-//       <Event
-//       EventName="nothing"
-//       is_Active={false}
-
-//       />
-//       <Event
-//       EventName="nothing"
-//       is_Active={false}
-
-//       />
-//       <Event
-//       EventName="nothing"
-//       is_Active={false}
-
-//       />
-//       <Event
-//       EventName="nothing"
-//       is_Active={false}
-
-//       />
-//       <Event
-//       EventName="nothing"
-//       is_Active={false}
-
-//       />
-      
-
-//       </Grid>
-
-//     </>
-//   )
-// }
-
-// export default EventSection
-
-
-
-
-
-
 import Button from "components/Button";
 import EventCard from "components/event_card";
 import Link from "next/link";
@@ -234,24 +79,7 @@ const Event = ({ EventName, is_Active, regCost, date, imgLink,siteLink , is_Team
     return (
       <>
         <Grid>
-        {/* <Event
-        EventName="REVELATION"
-        is_Active={true}
-        regCost="Rs. 500"
-        regLink="https://revelationreg.elecxia.live/"
-        date="NOV 05, 2022"
-        imgLink="./img/posters/revelation.jpeg"
-        // is_closed={true}
-        /> */}
-<!--         <Event
-        EventName=""
-        is_Active={true}
-        regCost="Rs. 150"
-        regLink="https://www.yepdesk.com/electrical-vehicles-designing"
-        date="NOV 05, 2022"
-        imgLink="./img/posters/ev workshop.jpeg"
-        // is_closed={true}
-        />
+            
             <Event
         EventName="ELECTRIC VEHICLES DESIGN WORKSHOP"
         is_Active={true}
@@ -262,41 +90,51 @@ const Event = ({ EventName, is_Active, regCost, date, imgLink,siteLink , is_Team
         // is_closed={true}
         />
             <Event
-        EventName="ELECTRIC VEHICLES DESIGN WORKSHOP"
+        EventName="E-KNOT -SOLDERING COMPETITION"
         is_Active={true}
         regCost="Rs. 150"
-        regLink="https://www.yepdesk.com/electrical-vehicles-designing"
+        regLink="https://www.yepdesk.com/soldering-competition"
         date="NOV 05, 2022"
-        imgLink="./img/posters/ev workshop.jpeg"
-        // is_closed={true}
-        />
-            <Event
-        EventName="ELECTRIC VEHICLES DESIGN WORKSHOP"
-        is_Active={true}
-        regCost="Rs. 150"
-        regLink="https://www.yepdesk.com/electrical-vehicles-designing"
-        date="NOV 05, 2022"
-        imgLink="./img/posters/ev workshop.jpeg"
-        // is_closed={true}
-        />
-            <Event
-        EventName="ELECTRIC VEHICLES DESIGN WORKSHOP"
-        is_Active={true}
-        regCost="Rs. 150"
-        regLink="https://www.yepdesk.com/electrical-vehicles-designing"
-        date="NOV 05, 2022"
-        imgLink="./img/posters/ev workshop.jpeg"
+        imgLink="./img/posters/SOLDERING-1.png"
         // is_closed={true}
         />
          <Event
-        EventName="ELECTRIC VEHICLES DESIGN WORKSHOP"
+        EventName="REVELATION"
+        is_Active={true}
+        regCost="Rs. 500"
+        regLink="https://www.yepdesk.com/revelation-inter-college-project-competition"
+        date="NOV 05, 2022"
+        imgLink="./img/posters/Revelation poster-3.png"
+        // is_closed={true}
+        /> 
+         <Event
+        EventName="SIMULATE- CIRCUIT SIMULATION"
+        is_Active={true}
+        regCost="Rs. 50"
+        regLink="https://www.yepdesk.com/circuit-simulation"
+        date="NOV 05, 2022"
+        imgLink="./img/posters/CIRCUIT STIMULATION-1.png"
+        // is_closed={true}
+        />
+            <Event
+        EventName="SPARK PITCH -IDEA PITCHING COMPETITION"
+        is_Active={true}
+        regCost="Rs. 50"
+        regLink="https://www.yepdesk.com/idea-pitching-competition"
+        date="NOV 05, 2022"
+        imgLink="./img/posters/IDEA PITCHING-1.png"
+        // is_closed={true}
+        />
+       
+         <Event
+        EventName="WIRZZ - WIRING MASTER"
         is_Active={true}
         regCost="Rs. 150"
-        regLink="https://www.yepdesk.com/electrical-vehicles-designing"
+        regLink="https://www.yepdesk.com/wiring-master"
         date="NOV 05, 2022"
-        imgLink="./img/posters/ev workshop.jpeg"
+        imgLink="./img/posters/WIRING-MASTER-1.png"
         // is_closed={true}
-        /> -->
+        /> 
 
       
 
